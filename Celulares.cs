e9e8303b79246f404fdace628408941bff617bb8
unknown <rafacs1404@gmail.com>
2020-05-27 08:50:33 -0300

    Classe Celulares criada

--- Celulares.cs
+++ Celulares.cs
@@ -1,7 +1,48 @@
 namespace _AulaPOOCelular
 {
-    public class Celulares
-    {
+    public class Celulares{
         
+    public string cor;
+    public string modelo;
+    public int tamanho;
+    public bool ligado;
+    public string ligação;
+    public string envioMensagem;
+    public string opçãoDesligar;
+    public string inválido;
+   
+    public bool Ligar(){
+
+        if(ligado == false){
+               return false;
+        }else{
+               return true;
+        }
+    }
+    public string Desligar(){
+            
+        string resultado1 = opçãoDesligar;
+        if(ligado == true){
+            return opçãoDesligar;
+        }else{
+            return inválido;
+        }
+    }
+    public string FazerLigação(){
+        string resultado2 = ligação;
+        if(ligado == true){
+            return resultado2;
+        }else{
+            return inválido;
+        }
+    }
+    public string EnviarMensagem(){
+        string resultado3 = envioMensagem; 
+        if(ligado == true){
+                return resultado3;
+            }else{    
+                return inválido;
+            }    
+        }    
     }
 }
